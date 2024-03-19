ceef95d45820d2b78145c332d859ef4207147ec0
Samuele Pignone <s4838155@studenti.unige.it>
2024-03-19 17:44:53 +0100

    Fixed the animation in map

--- src/components/section1/Section1.jsx
+++ src/components/section1/Section1.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useRef, useState } from 'react';
 import * as d3 from 'd3';
-import europemap from '../../map/europe.json';
+import { BarList } from 'reaviz';
+import europemap from '../../map/europe_cleaned.json';
+import { FaCirclePlay, FaCircleStop } from "react-icons/fa6";
 
 function Section1() {
     const ref = useRef();
+    const intervalRef = useRef(null);
     const [dimensions, setDimensions] = useState({ // Defaults
         width: 960,
         height: 600,
@@ -11,6 +14,32 @@ function Section1() {
     const [accessData, setAccessData] = useState([]);
     const [selectedYear, setSelectedYear] = useState("2023");
     const [loading, setLoading] = useState(false);
+    const [animation, setAnimation] = useState(false);
+
+    // start an animation from 2002 to 2023
+    // for each year, update the selected year
+    // use a timeout to wait between years
+    const play = () => {
+        setAnimation(true);
+        setSelectedYear("2002");
+        let year = 2003;
+        intervalRef.current = setInterval(() => {
+            setSelectedYear(year.toString());
+            year++;
+            if (year > 2023) {
+                clearInterval(intervalRef.current);
+                setAnimation(false);
+            }
+        }, 1000);
+    }
+
+    // stop the animation
+    const stop = () => {
+        if (intervalRef.current) {
+            clearInterval(intervalRef.current);
+            setAnimation(false);
+        }
+    }
 
     // Effect hook to handle window resize
     useEffect(() => {
@@ -102,15 +131,11 @@ function Section1() {
 
         const yearAccessData = accessData.map(d => d[selectedYear]);
         const colorScale = d3.scaleSequential(d3.interpolateBlues)
-            .domain(d3.extent(yearAccessData));
+            .domain([0, 100]);
 
         const accessByCountry = new Map(accessData.map(d => [d.country, d[selectedYear]]));
 
         d3.select(ref.current).selectAll('.country')
-            .attr('fill', d => {
-                const access = accessByCountry.get(d.properties.ISO2);
-                return access ? colorScale(access) : '#ccc';
-            })
             .on('mouseover', function (event, d) {
                 // show tooltip
                 const access = accessByCountry.get(d.properties.ISO2);
@@ -120,7 +145,7 @@ function Section1() {
                     .duration(200)
                     .style('opacity', .9);
                 tooltip.html(`<p style='color: #1e81b0'>${d.properties.NAME}</p>
-                                <p style='color: #1e81b0'>${access ? access.toString()+'%' : 'No data'}</p>`)
+                            <p style='color: #1e81b0'>${access ? access.toString() + '%' : 'No data'}</p>`)
                     .style('left', (event.pageX) + 'px')
                     .style('top', (event.pageY - 28) + 'px');
             })
@@ -134,8 +159,15 @@ function Section1() {
                         .on('end', () => tooltip.style('display', 'none'));
                 }
 
-            });
-    }, [selectedYear, accessData]);
+            })
+            // update fill color with a transition to the new color
+            .transition()
+            .duration(500)
+            .attr('fill', d => {
+                const access = accessByCountry.get(d.properties.ISO2);
+                return access ? colorScale(access) : '#ccc';
+            })
+    }, [selectedYear, accessData, dimensions]);
 
     const handleYearChange = (event) => setSelectedYear(event.target.value);
 
@@ -148,8 +180,41 @@ function Section1() {
                 })}
             </select>
             {loading && <p>Loading...</p>}
+
             <div ref={ref} style={{ width: '100%', height: '100%', display: loading ? 'none' : 'block' }}></div>
             <div id='tooltip' className='absolute bg-white border border-gray-300 shadow-lg p-2 rounded-md opacity-0 hidden' onMouseLeave={() => d3.select('#tooltip').style('display', 'none')}></div>
+            <div className='w-full flex flex-col justify-center items-center'>
+                {
+                    animation ? (
+                        <>
+                            <p>Year: {selectedYear}</p>
+                            <div style={{ textAlign: 'center', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                <p className='mr-3 font-bold'>2002</p>
+                                <BarList
+                                    style={{ width: 350 }}
+                                    data={[{
+                                        key: "",
+                                        data: (selectedYear - 2002) * 100 / 21
+                                    }]}
+                                    type='percent'
+                                />
+                                <p className='ml-3 font-bold'>2023</p>
+                            </div>
+                            <div onClick={stop} className=''>
+                                <FaCircleStop className='text-3xl cursor-pointer' />
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                            <p></p>
+                            <div onClick={play} className=''>
+                                <FaCirclePlay className='text-3xl cursor-pointer' />
+                            </div>
+                            <p className='mt-2'>Start an animation from 2002 to 2023</p>
+                        </>
+                    )
+                }
+            </div>
         </div>
     );
 }
